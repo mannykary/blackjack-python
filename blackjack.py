a2ed6f9603a1d9c62b8a8ffd13284044ac30622a
mannykary <mrkaryam@gmail.com>
2014-07-17 00:51:16 -0700

    refactored code to make it cleaner and easier to read

--- blackjack.py
+++ blackjack.py
@@ -1,99 +1,156 @@
 import random
 import sys
 
-def main():
-  suits = ('H S D C').split()
+class Player:
+  def __init__(self):
+    self.name = None
+    self.hand = None
+
+  def name(self, n):
+    self.name = n
+
+  def hand(self, h):
+    self.hand = h
+
+player = Player()
+dealer = Player()
+
+def initialize():
+  player.name = get_player_name()
+  dealer.name = 'Dealer'
+  start_round()
+
+def start_round():
+  deck = get_new_deck()
+  [player.hand, dealer.hand] = deal(deck)
+  display_hand_and_score(player)
+
+  if has_blackjack(player.hand):
+    if has_blackjack(dealer.hand):
+      print "Both have blackjack! Push!"
+    else:
+      print "You have blackjack!"
+
+    display_hand_and_score(dealer)
+    #ask_to_deal_again()
+
+  else:
+    stand = False
+
+    while score(player.hand) < 21 and stand == False:
+      next_step = hit_or_stand()
+
+      if next_step == 'H':
+        hit(deck, player.hand)
+        display_hand_and_score(player)
+
+        if is_busted(player.hand):
+          print "You have busted."
+          #ask_to_deal_again()
+        elif score(player.hand) == 21:
+          keep_hitting_dealer(deck, dealer.hand)
+          if score(dealer.hand) == 21:
+            print "Push!"
+          else:
+            print "You win!"
+          #ask_to_deal_again()
+
+      elif next_step == 'S':
+        stand = True
+        keep_hitting_dealer(deck, dealer.hand)
+        if score(player.hand) > score(dealer.hand):
+          print "You win!"
+        elif score(player.hand) == score(dealer.hand):
+          print "Push!"
+        else:
+          print "Dealer wins."
+        #ask_to_deal_again()
+
+      else:
+        print "Please enter H to hit, or S to stand."
+
+  ask_to_deal_again()
+
+def hit_or_stand():
+  response = raw_input("Hit (H) or Stand (S)? ")
+  return response.upper()
+
+def is_busted(hand):
+  return score(hand) > 21
+
+def keep_hitting_dealer(deck, hand):
+  display_hand_and_score(dealer)
+
+  while score(hand) < 17:
+
+    hit(deck, hand)
+    display_hand_and_score(dealer)
+
+    if is_busted(dealer.hand):
+      print "Dealer has busted. You win!"
+      ask_to_deal_again()
+
+def get_player_name():
+  player_name = raw_input("Welcome to Manny's Casino! Please enter your name.\n")
+
+  while player_name == '':
+    player_name = raw_input("You did not enter a name! Please enter your name.\n")
+
+  return player_name
+
+def ask_to_deal_again():
+  response = raw_input("Deal again? (Y/N) ")
+  if response.upper() == 'Y':
+    start_round()
+  elif response.upper() == 'N':
+    sys.exit()
+  else:
+    print "Please enter Y to deal again, or N to exit."
+    ask_to_deal_again()
+
+def deal(deck):
+  print "\nDealing cards..."
+  player = [deck.pop(), deck.pop()]
+  dealer = [deck.pop(), deck.pop()]
+  return [player, dealer]
+
+def has_blackjack(hand):
+  return score(hand) == 21
+
+def hit(deck, hand):
+  hand.append(deck.pop())
+
+def display_hand_and_score(player_obj):
+  print player_obj.name + ' has: ' + str(player_obj.hand) + ' (Score: ' + str(score(player_obj.hand)) + ')'
+
+def get_new_deck():
+  suits = ('Hearts Spades Diamonds Clubs').split()
   ranks = ('2 3 4 5 6 7 8 9 10 J Q K A').split()
   deck = []
 
   for i in suits:
     for j in ranks:
-      deck.append(j + ':' + i)
+      deck.append(j + ' of ' + i)
 
   random.shuffle(deck)
-  print deck
 
-  [p, d] = deal(deck)
-
-  print "You have: " + str(p)
-  print "Your score is " + str(score(p))
-
-  if score(p) == 21:
-    var = raw_input("You have won! Deal again? Y/N ")
-    deal_again(var)
-
-  stand = False
-
-  while score(p) < 21 and stand == False:
-    var = raw_input("Hit (H) or Stand (S)? ")
-
-    if var.upper() == 'H':
-      hit(deck, p)
-      print "You have: " + str(p)
-      print "Your score is " + str(score(p))
-
-      if score(p) > 21:
-        var = raw_input("You have busted. Deal again? Y/N ")
-        deal_again(var)
-
-      if score(p) == 21:
-        var = raw_input("You have won! Deal again? Y/N ")
-        deal_again(var)
-
-    elif var.upper() == 'S':
-      stand = True
-      print "Dealer has: " + str(d) + " (score: " + str(score(d)) + ")"
-
-      while score(d) < 17:
-        hit(deck, d)
-        print "Dealer hits, now has: " + str(d) + " (score: " + str(score(d)) + ")"
-
-        if score(d) > 21:
-          var = raw_input("Dealer has busted. You have won! Deal again? Y/N ")
-          deal_again(var)
-        elif score(d) >= 17:
-          if score(d) == score(p):
-            var = raw_input("You and dealer have same score. Push! Deal again? Y/N ")
-            deal_again(var)
-          elif score(p) > score(d):
-            var = raw_input("You beat the dealer! Deal again? Y/N ")
-            deal_again(var)
-          elif score(p) < score(d):
-            var = raw_input("You lost to the dealer. Deal again? Y/N ")
-            deal_again(var)
-
-    else:
-      print "Please enter H to hit, or S to stand."
-
-def deal_again(var):
-  if var.upper() == 'Y':
-    main()
-  else:
-    sys.exit()
-
-def deal(deck):
-  player = [deck.pop(), deck.pop()]
-  dealer = [deck.pop(), deck.pop()]
-  return [player, dealer]
-
-def hit(deck, hand):
-  hand.append(deck.pop())
+  return deck
 
 def score(hand):
 
   score = 0
-  num_aces = 0
+  number_of_aces = 0
 
   for i in hand:
-    rank = i.split(':')[0]
+    rank = i.split(' of ')[0]
     if rank in ('J', 'Q', 'K'):
       score += 10
     elif rank == 'A':
         score += 11  # need to add in logic to change ace value from 11 to 1 as needed.
-        num_aces += 1
+        number_of_aces += 1
     else:
       score += int(rank)
 
   return score
 
-main()
+initialize()
