6b6cb4780e42d91effc9a0a46fb9448f4fba08a1
mannykary <mrkaryam@gmail.com>
2014-07-17 21:54:59 -0700

    added betting

--- blackjack.py
+++ blackjack.py
@@ -5,6 +5,7 @@ class Player:
   def __init__(self):
     self.name = None
     self.hand = None
+    self.chips = 0  #change to 100 in final version.
 
   def name(self, n):
     self.name = n
@@ -12,24 +13,37 @@ class Player:
   def hand(self, h):
     self.hand = h
 
+  def chips(self, c):
+    self.chips = c
+
 player = Player()
 dealer = Player()
 
 def initialize():
   player.name = get_player_name()
+  player.chips = 100
   dealer.name = 'Dealer'
   start_round()
 
 def start_round():
   deck = get_new_deck()
+
+  if player.chips > 0:
+    player_bet = receive_bet()
+  else:
+    print "You have no more chips!"
+    ask_to_play_again()
+
   [player.hand, dealer.hand] = deal(deck)
   display_hand_and_score(player)
 
   if has_blackjack(player.hand):
     if has_blackjack(dealer.hand):
       print "Both have blackjack! Push!"
+      player.chips += player_bet
     else:
       print "You have blackjack!"
+      player.chips += 2*player_bet
 
     display_hand_and_score(dealer)
     #ask_to_deal_again()
@@ -48,20 +62,24 @@ def start_round():
           print "You have busted."
           #ask_to_deal_again()
         elif score(player.hand) == 21:
-          keep_hitting_dealer(deck, dealer.hand)
+          keep_hitting_dealer(deck, dealer.hand, player_bet)
           if score(dealer.hand) == 21:
             print "Push!"
+            player.chips += player_bet
           else:
             print "You win!"
+            player.chips += 2*player_bet
           #ask_to_deal_again()
 
       elif next_step == 'S':
         stand = True
-        keep_hitting_dealer(deck, dealer.hand)
+        keep_hitting_dealer(deck, dealer.hand, player_bet)
         if score(player.hand) > score(dealer.hand):
           print "You win!"
+          player.chips += 2*player_bet
         elif score(player.hand) == score(dealer.hand):
           print "Push!"
+          player.chips += player_bet
         else:
           print "Dealer wins."
         #ask_to_deal_again()
@@ -71,6 +89,26 @@ def start_round():
 
   ask_to_deal_again()
 
+def receive_bet():
+  print "You have " + str(player.chips) + " chips."
+
+  bet = raw_input("Please enter the number of chips to bet. ")
+
+  try:
+    if int(bet) < 1:
+      print "The minimum bet is 1 chip."
+      receive_bet()
+    elif int(bet) > player.chips:
+      print "You do not have that many chips!"
+      receive_bet()
+    else:
+      player.chips -= int(bet)
+      return int(bet)
+
+  except TypeError:
+    print "You did not enter a valid bet."
+    receive_bet()
+
 def hit_or_stand():
   response = raw_input("Hit (H) or Stand (S)? ")
   return response.upper()
@@ -78,7 +116,7 @@ def hit_or_stand():
 def is_busted(hand):
   return score(hand) > 21
 
-def keep_hitting_dealer(deck, hand):
+def keep_hitting_dealer(deck, hand, bet):
   display_hand_and_score(dealer)
 
   while score(hand) < 17:
@@ -88,6 +126,7 @@ def keep_hitting_dealer(deck, hand):
 
     if is_busted(dealer.hand):
       print "Dealer has busted. You win!"
+      player.chips += 2*bet
       ask_to_deal_again()
 
 def get_player_name():
@@ -107,6 +146,16 @@ def ask_to_deal_again():
   else:
     print "Please enter Y to deal again, or N to exit."
     ask_to_deal_again()
+
+def ask_to_play_again():
+  response = raw_input("Play again? (Y/N) ")
+  if response.upper() == 'Y':
+    initialize()
+  elif response.upper() == 'N':
+    sys.exit()
+  else:
+    print "Please enter Y to play again, or N to exit."
+    ask_to_play_again()
 
 def deal(deck):
   print "\nDealing cards..."
@@ -159,6 +208,7 @@ def score(hand):
   #       break
   #     else:
   #       i -= 1
+
   if score > 21 and number_of_aces > 0:
     for i in range(number_of_aces, 0, -1):
       score -=10
